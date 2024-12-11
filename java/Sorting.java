e350d588b809ca63040f37947bebdd27f8504502
om-weye <omraj.sharma@wheelseye.com>
2024-12-11 23:06:19 +0530

    sorting

--- java/Sorting.java
+++ java/Sorting.java
@@ -1,4 +1,59 @@
-package PACKAGE_NAME;
-
 public class Sorting {
+    public static void main(String[] args) {
+
+    }
+
+    // ASSIGNMENT - WHY IT'S TIME COMPLEXITY IS N^2
+
+    /**
+     * n = 10
+     *
+     * N * N = O(N^2)
+     */
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    /**
+     * tc - O(N^2)
+     */
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+
+            System.out.println(java.util.Arrays.toString(arr));
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+
+            int j = i - 1;
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+
+            arr[j + 1] = temp;
+        }
+    }
 }
