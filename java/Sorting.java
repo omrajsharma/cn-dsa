8e0f02d00f97e7a1056a6798a40e82ef00b14600
om-weye <omraj.sharma@wheelseye.com>
2024-12-14 21:33:15 +0530

    merge sort

--- java/Sorting.java
+++ java/Sorting.java
@@ -1,7 +1,73 @@
 public class Sorting {
     public static void main(String[] args) {
+        int[] arr = {12,11,13,7,6,5};
 
+        System.out.println("Original Array: " + java.util.Arrays.toString(arr));
+
+        mergeSort(arr, 0, arr.length-1);
+
+        System.out.println("Sorted Array: " + java.util.Arrays.toString(arr));
     }
+
+    // sorts the array
+    private static void mergeSort(int[] arr, int left, int right) {
+        // arr = {12,11,13,7,6,5}
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            mergeSort(arr, left, mid);              // 11, 12, 13, 7, 6, 5
+            mergeSort(arr, mid + 1, right);     // 11, 12, 13, 5, 6, 7
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        // 11, 12, 13, 5, 6, 7
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArr = new int[n1];        // 0, 0, 0
+        int[] rightArr = new int[n2];       // 0, 0, 0
+
+        for (int i = 0; i < n1; i++) {
+            leftArr[i] = arr[left + i];
+        }
+        // leftArr = 11, 12, 13
+
+        for (int j = 0; j < n2; j++) {
+            rightArr[j] = arr[mid + 1 + j];
+        }
+        // rightArr = 5, 6, 7
+
+        // sort two sorted array into one array
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            }
+            else {
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
+    }
+
+
 
     // ASSIGNMENT - WHY IT'S TIME COMPLEXITY IS N^2
 
